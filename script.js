e335e4980313ec82a83f7d949fd7675c2671b2fd
joaoengstersoares-spec <joao.engster.soares@escola.pr.gov.br>
2026-06-28 18:57:59 -0300

    Refactor image interaction and add message display
    Removed image event listeners and added a function to display a message about agribusiness.

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-const imagens = document.querySelectorAll(".card img");
+function mostrarMensagem(){
 
-imagens.forEach(img => {
+    document.getElementById("mensagem").innerHTML =
+    "🌱 O agronegócio é essencial para alimentar milhões de pessoas, gerar empregos e fortalecer a economia do país.";
 
-img.addEventListener("mousemove", (e)=>{
-
-const x = (e.offsetX/img.clientWidth -0.5)*20;
-const y = (e.offsetY/img.clientHeight -0.5)*20;
-
-img.style.transform = `
-perspective(600px)
-rotateY(${x}deg)
-rotateX(${-y}deg)
-scale(1.12)
-`;
-
-});
-
-img.addEventListener("mouseleave", ()=>{
-
-img.style.transform="perspective(600px) rotateX(0) rotateY(0) scale(1)";
-
-});
-
-});
+}
